f062a43c52254715420becc7b6987312882598fa
Rodrigo Carvalho <rhrc09@gmail.com>
2022-09-11 11:31:30 -0300

    feat: add authentication with GitHub provider

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,38 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  useEffect(() => {
+    if (data?.user && data?.expires) {
+      const {
+        user: { name, email },
+        expires,
+      } = data;
+      console.log({ name, email, expires });
+    }
+  }, [data, data?.user]);
+
+  return data?.user ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Rodrigo Henrique
+      {data?.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
